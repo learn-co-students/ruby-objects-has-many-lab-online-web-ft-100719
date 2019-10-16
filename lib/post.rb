545276642028892d686ff76bb51168cd7faeb460
Kathleen Kamali <kathleenkamali@gmail.com>
2019-10-16 16:55:59 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,22 @@
+class Post 
+  attr_accessor :title, :author 
+  
+  @@all = [] 
+  
+  def initialize(title)
+    self.title = title
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def author_name 
+    if self.author != nil 
+      return self.author.name
+    else
+      return nil 
+    end
+  end 
+end 
