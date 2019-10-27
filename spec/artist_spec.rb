cf1f7619d2936ba62cddf570a6ae6c79a03e9033
Prince Charles <princecharles35@yahoo.com>
2019-10-27 17:42:54 +0000

    Done.

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -1,50 +1,30 @@
-require "spec_helper"
+class Artist
+  attr_accessor :name
+  @@song_count = 0
+  def initialize(name)
+    @name = name
+    @songs = [] #empty collection of songs
+  end
 
-describe "Artist" do
+  def songs
+    @songs
+  end
 
-  let!(:adele) { Artist.new("Adele") }
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
 
-    describe "#new" do
-      it "is initialized with a name" do
-        expect{Artist.new("Beyonce")}.to_not raise_error
-      end
-    end
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
 
-    describe "#name" do
-      it "has an attr_accessor for name" do
-        expect(adele.name).to eq("Adele")
-      end
-    end
+  def self.song_count
+    @@song_count
+  end
 
-    describe "#songs" do
-      it "has many songs" do
-        expect(adele.songs).to be_a(Array)
-        hello = Song.new("Hello")
-        hello.artist = adele
-        expect(adele.songs).to eq([hello])
-      end
-    end
-
-    describe "#add_song" do
-      it "takes in an argument of a song and associates that song with the artist by telling the song that it belongs to that artist" do
-        hello = Song.new("Hello")
-        adele.add_song(hello)
-        expect(adele.songs).to include(hello)
-        expect(hello.artist).to eq(adele)
-      end
-    end
-
-    describe "#add_song_by_name" do
-      it "takes in an argument of a song name, creates a new song with it and associates the song and artist" do
-        adele.add_song_by_name("Rolling in the Deep")
-        expect(adele.songs.last.name).to eq("Rolling in the Deep")
-        expect(adele.songs.last.artist).to eq(adele)
-      end
-    end
-
-    describe ".song_count" do
-      it "is a class method that returns the total number of songs associated to all existing artists" do
-        expect(Artist.song_count).to eq(3)
-      end
-    end
-end
+end 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,32 +1,30 @@
-require_relative "../lib/artist.rb"
-require_relative "../lib/song.rb"
-require_relative "../lib/author.rb"
-require_relative "../lib/post.rb"
-
-# binding.pry
-
-RSpec.configure do |config|
-  # rspec-expectations config goes here. You can use an alternate
-  # assertion/expectation library such as wrong or the stdlib/minitest
-  # assertions if you prefer.
-  config.expect_with :rspec do |expectations|
-    # This option will default to `true` in RSpec 4. It makes the `description`
-    # and `failure_message` of custom matchers include text for helper methods
-    # defined using `chain`, e.g.:
-    #     be_bigger_than(2).and_smaller_than(4).description
-    #     # => "be bigger than 2 and smaller than 4"
-    # ...rather than:
-    #     # => "be bigger than 2"
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+class Artist
+  attr_accessor :name
+  @@song_count = 0
+  def initialize(name)
+    @name = name
+    @songs = [] #empty collection of songs
   end
 
-  # rspec-mocks config goes here. You can use an alternate test double
-  # library (such as bogus or mocha) by changing the `mock_with` option here.
-  config.mock_with :rspec do |mocks|
-    # Prevents you from mocking or stubbing a method that does not exist on
-    # a real object. This is generally recommended, and will default to
-    # `true` in RSpec 4.
-    mocks.verify_partial_doubles = true
+  def songs
+    @songs
   end
 
-end
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def self.song_count
+    @@song_count
+  end
+
+end 
